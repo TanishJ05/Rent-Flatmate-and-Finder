7467cf0e91d908737912aeb91d5369485b270448
TanishJ05 <tanishj0522@gmail.com>
2026-07-15 03:06:09 +0530

    update

--- client/src/api/axiosInstance.ts
+++ client/src/api/axiosInstance.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 axios.defaults.withCredentials = true;
 
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5001/api",
+  baseURL: '/api', //|| "http://localhost:5001/api",
   withCredentials: true,
 });
 
